19f351850f0d461640ca58821cfb7b2fff21ac8f
Jamila <jamnjeri@gmail.com>
2022-12-02 17:15:22 +0300

    Completed Assignmnet

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+//Adding an Event Listener
 function addingEventListener() {
+    const input = document.getElementById("button")
+    
+    function clickAlert() {
+        alert('I was clicked!');
+    };
+    
+    input.addEventListener('click', clickAlert )
 }
+
+addingEventListener()
+
